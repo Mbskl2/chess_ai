c0dd1ca56cb61bfec25f7fc5b55e2809d762ea8d
Michal Banach <mbskl2@pm.me>
2024-09-03 21:19:54 +0200

    Show a chessboard.

--- chess.py
+++ chess.py
@@ -1,5 +1,19 @@
-field_color_black = (177, 110, 65)
-field_color_white = (255, 213, 153)
+import tkinter as tk
+
+field_color_black = '#b16e41'
+field_color_white = '#ffd599'
+field_size = 100
+
+window = tk.Tk()
+canvas = tk.Canvas(window, width=field_size * 8, height=field_size * 8)
+canvas.pack()
+
+for i in range(8):
+    for j in range(8):
+        color = [field_color_black, field_color_white][(i+j) % 2]
+        canvas.create_rectangle(i*field_size, j*field_size, (i+1)*field_size, (j+1)*field_size, fill=color)
+
+window.mainloop()
 
 if __name__ == '__main__':
     print('Hello World')
